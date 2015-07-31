34232eb671c16af8249a6d9950c4561067860972
杨勇 <yangyong@loukou.com>
2015-07-31 14:05:04 +0800

    fix exception

--- src/main/java/com/loukou/mapi/servlet/MapiDispatcherServlet.java
+++ src/main/java/com/loukou/mapi/servlet/MapiDispatcherServlet.java
@@ -1,8 +1,11 @@
 package com.loukou.mapi.servlet;
 
+import java.io.IOException;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
 import org.springframework.web.servlet.DispatcherServlet;
 
 import com.dianping.cat.Cat;
@@ -15,10 +18,12 @@ import com.loukou.mapi.context.ContextProcessor;
 public class MapiDispatcherServlet extends DispatcherServlet {
 	public static final String MAPI_CONTEXT = "mapi_context";
 	public static final String MAPI_TRANSACTION_NAME = "MAPI";
-
+	private static final Logger logger = Logger.getLogger(MapiDispatcherServlet.class);
 	@Override
 	protected void doDispatch(HttpServletRequest request,
-			HttpServletResponse response) throws Exception {
+			HttpServletResponse response){
+		//设置编码，否则会乱码
+		response.setCharacterEncoding("utf-8");
 		// 获取request header 中的公共参数 context
 		Context context = ContextProcessor.getContext(request);
 		request.setAttribute(MAPI_CONTEXT, context);
@@ -35,7 +40,11 @@ public class MapiDispatcherServlet extends DispatcherServlet {
 		} catch (Exception e) {
 			Cat.getProducer().logError(e);// 用log4j记录系统异常，以便在Logview中看到此信息
 			t.setStatus(e);
-			throw e;
+			try {
+				response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "服务器繁忙，请稍后再试");
+			} catch (IOException e1) {
+				logger.info("response sendError IOException.");
+			}
 		} finally {
 			t.complete();
 		}
